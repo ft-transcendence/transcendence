688d5b3192ab2ef69776f238a62cc8f2c4e42f1e
shlu <shuyenlu@gmail.com>
2022-06-10 18:00:04 +0200

    Why :construction: temp commit -- break time
    *
    
    *

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/users/entities/users.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity, BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from 'bcrypt';
+
+@Entity('Users')
+export class UserEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    @BeforeInsert()
+    async hashPassword() {
+        if (this.password)
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    toResponseObject() {
+        return {id: this.id, username: this.username}
+    }
+}
