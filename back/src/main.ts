1b8bb6643ee799992c29d377ba98de8caca2ecf6
Florian CAVILLON <fcavillo@student.42.fr>
2022-06-19 15:16:33 +0200

    :card_file_box: added basic signin/signup logics
    *
    
    - detailed signin/signup functions into the auth.service
    - /!\ no hash here, just shells to welcome satch's code
    - added @unique param to the schema
    - added user/bookmark relation

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });  //instantiating the app + enabling CORS (Access-Control-Allow-Origin) 
   // app.enableCors();
 
+  app.useGlobalPipes(new ValidationPipe({           //instantiating the use of Pipes (converts or validates types)
+    whitelist: true                                 //only allows data defined in the dto to be received (security)
+  }));         
   await app.listen(4000);                           //launching server
 }
 bootstrap();
