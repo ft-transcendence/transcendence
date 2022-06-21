47868a996b8428c3977b57fef402ddec1223435a
shlu <shuyenlu@gmail.com>
2022-06-21 19:59:08 +0200

    add game field and bars

--- front/src/Game.tsx
+++ front/src/Game.tsx
@@ -10,9 +10,55 @@ const socket = io("ws://localhost:4000");
 export default function Game() {
     return (
         <div className='Radial-background'>
-              
-            <div className='Game-board'>
+            <div className='Page-top'>
 
+            </div>
+            <div className='Game-board'>
+                <div className='ball'>
+
+                </div>
+                <div className='Info-card'>
+                    <div className='Player-left'>
+                        <div className='Info-left'>
+                            <div className='photo'>
+                            
+                            </div>
+                            <div className='name'>
+                            
+                            </div>
+                        </div>
+                        <div className='Score'>
+                        
+                        </div>
+                    </div>
+                    <div className='Player-right'>
+                        <div className='Score'>
+                        
+                        </div>
+                        <div className='Info-right'>
+                            <div className='name'>
+                            
+                            </div>
+                            <div className='photo'>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='Field'>
+                    <div className='Pad'>
+                    <style>pedding-left: 30px;</style>
+                    </div>
+                    <div className='Center-line'>
+
+                    </div>
+                    <div className='Center-circle'>
+
+                    </div>
+                    <div className='Pad'>
+
+                    </div>
+                </div>
             </div>
             <div className='buttons'>
 
