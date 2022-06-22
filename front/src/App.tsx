fcddb1e242caa7abc2e88caaaf2823c2b66c2ad8
shuyenla <shuyenlu@gmail.com>
2022-06-22 20:18:06 +0200

    :wastebasket: rm the test code

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Hamburger from './Hamburger';
 import { io } from "socket.io-client";
 import { getImpliedNodeFormatForFile } from 'typescript';
 import { ServerResponse } from 'http';
