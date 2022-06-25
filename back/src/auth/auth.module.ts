d5ee852150352d7d31896be44d889f309e54db34
Satcheen SHAKYA <sshakya@student.42.fr>
2022-06-25 16:44:56 +0200

    :construction: building auth module
    *
    
    *

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 /* JASON WEB TOKEN AUTH MODULE */
 import { JwtModule } from "@nestjs/jwt";
+import { jwtStrategy } from "./strategy";
 
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, jwtStrategy],
 })
 
 export class AuthModule {}
