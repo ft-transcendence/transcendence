98c9103482d3366cf07a327e56018f447eaca340
Satcheen SHAKYA <sshakya@student.42.fr>
2022-06-26 12:50:13 +0200

    :construction: more Auth JWT
    *
    
    - read previous commit
    - setup Bearer token verification
    - add console logging
    *

--- back/src/auth/strategy/jwt.strategy.ts
+++ back/src/auth/strategy/jwt.strategy.ts
@@ -33,7 +33,7 @@ export class jwtStrategy extends PassportStrategy(
         console.log({
             data,
         })
-        return data;
+        return 'hi';
     }
 
 }
--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller,
+    Get,
+    Req,
+    UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
     @UseGuards(AuthGuard('jwt'))
     @Get('me') 
-    getMe() {
+    getMe(@Req() req: Request) {
+        console.log({
+            user: req.user,
+        })
         return 'user info here';
     }
 }
