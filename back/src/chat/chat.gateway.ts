510210329a5580de9cca0f639772bec8a1c5fca8
shlu <shuyenlu@gmail.com>
2022-06-28 22:10:06 +0000

    :test_cube: testing the back front interaction of socket io

--- back/src/chat/chat.gateway.ts
+++ back/src/chat/chat.gateway.ts
@@ -1,18 +1,30 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { from, map, Observable } from 'rxjs';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({cors: {
-    origin: "http://localhost:3000"}})
-  export class ChatGateway {
-
-    @WebSocketServer()
-    server: Server;
-
-    @SubscribeMessage('chat')
-    handleChat(@MessageBody() data: string, 
-    @ConnectedSocket() client: Socket): string {
-      return data;
-      client.emit('sent!')
-    }
-  }
+@WebSocketGateway(
+  {cors: {origin: "http://localhost:3000" } }
+)
+    
+export class ChatGateway {
   
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('msg')
+  handleMsg(@MessageBody() data: string, 
+  @ConnectedSocket() client: Socket): string {
+    return data;
+    client.emit('sent!')
+  }
+
+  @SubscribeMessage('msg')
+  onMsg(@MessageBody() data: string):Observable <WsResponse<number>> {
+    const event = 'msg';
+    const response = [1, 2, 3];
+
+    return from(response).pipe(
+      map(data => ({ event, data })),
+    )
+  }
+}
--- front/src/chat/Chat.tsx
+++ front/src/chat/Chat.tsx
@@ -1,13 +1,46 @@
-import { io } from 'socket.io-client';
+import React, { useEffect } from "react";
 import "./Chat.css";
+import { io } from 'socket.io-client';
 import { Link, Outlet, useLocation } from "react-router-dom";
+import { createSocket } from "dgram";
 
-const socket = io("ws:")
+const socket = io('http://localhost:3000');
 
 export default function Chat() {
+    
+    socket.emit('msg', {name: 'Nest' }, (data: string) => console.log(data));
+    // socket.on('msg', (data: string) => console.log(data));
+    
+    useEffect(() => {
+        socket.on('connect', function() {
+            console.log('Connected');
 
-    socket.emit('chat', {name: 'Nest' }, (data: string) => console.log(data));
-    socket.on('chat', (data: string) => console.log(data));
+            socket.emit('msg', {test: 'test'});
+        });
+
+        socket.on('msg', function(data) {
+            console.log('msg', data);
+        });
+
+        socket.on('exception', function(data) {
+            console.log('msg', data);
+        });
+
+        socket.on('disconnect', function(data) {
+            console.log('Disconnected');
+        });
+
+        return () => {
+            socket.off('connect');
+            socket.off('msg');
+            socket.off('exception');
+            socket.off('disconnect');
+        }
+
+    }, []);
+
+    
+
     return (
         <div>
             this is chat hehehe
