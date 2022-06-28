ecafeef18155e98e95124cec531c048c6e3c5df8
shlu <shuyenlu@gmail.com>
2022-06-28 19:41:15 +0000

    :hammer: update the router for game

--- front/src/routes/GamePage.tsx
+++ front/src/routes/GamePage.tsx
@@ -3,9 +3,7 @@ import Game from "../game/Game";
 export default function GamePage() {
     return (
       <div>
-        {/* <main style={{ padding: "1rem 0" }}> */}
         <Game></Game>
-      {/* </main> */}
       </div>
     );
   }
