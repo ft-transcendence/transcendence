d15994380073799535b394d319a0841e459b3df2
Marie-lou VALDES <mvaldes@student.42.fr>
2022-06-30 15:08:04 +0200

    🎨 sign up form style change for form only
    Found a way to customize bootstrap and change colors shapes etc for the
    form. Will be tacling buttons right after.

--- front/src/routes/Auth.tsx
+++ front/src/routes/Auth.tsx
@@ -5,7 +5,7 @@ export default function Auth(props:any) {
   return (AuthForm());
 }
 
-function AuthForm() {
+const  AuthForm = () => {
 
   let [authMode, setAuthMode] = useState("signin")
 
@@ -19,44 +19,50 @@ function AuthForm() {
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">{authMode === "signin" ? "Sign Up": "Sign In"}</h3>
           <div className="text-center">
-            Already registered?{" "}
+            Already registered?&nbsp;
             <span className="link-primary" onClick={changeAuthMode}>
-              {authMode === "signin" ? "Sign In": "Sign Up"}
+              {authMode === "signin" ? "Sign in.": "Sign up."}
             </span>
           </div>
+
           {authMode == "signup" &&
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Username</Form.Label>
+              <Form.Label className="form-test" >USERNAME</Form.Label>
               <Form.Control
               type="text"
               placeholder="JaneDoe"
               />
             </Form.Group>
           }
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
+
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+            <Form.Label className="form-test">EMAIL</Form.Label>
             <Form.Control
             type="email"
             placeholder="name@example.com"
             />
-          </Form.Group>
-          <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-          <Form.Control
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-            placeholder="****"
-          />
-          <Form.Text id="passwordHelpBlock" muted>
-            Your password must be 8-20 characters long.
-          </Form.Text>
+          </Form.Group >
+          
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+            <Form.Label className="form-test" htmlFor="inputPassword1">PASSWORD</Form.Label>
+            <Form.Control
+              type="password"
+              id="inputPassword5"
+              aria-describedby="passwordHelpBlock"
+              placeholder="****"
+            />
+            <Form.Text className="form-help-block" id="passwordHelpBlock" muted>
+              Your password must be 8-20 characters long.
+            </Form.Text>
+          </Form.Group >
+          
           <div className="d-grid gap-2 mt-3">
-            <button as="input" type="submit">
+            <button type="submit">
               Submit
             </button>
           </div>
           <p className="text-center mt-2">
-            Forgot <a href="#">password?</a>
+            Forgot your &nbsp; <a href="#">password?</a>
           </p>
         </div>
       </form>
