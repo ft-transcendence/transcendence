e04410447c6334a6fd9a20c5b725656cf5f29f68
sshakya <sshakya@student.42.fr>
2022-07-08 10:42:43 +0200

    :construction: temp commit
    *
    
    *

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,6 @@ import { PrismaService } from './prisma/prisma.service';
 /* Start the app */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
   // Enable CORS
   app.enableCors();
 
@@ -23,16 +22,3 @@ async function bootstrap() {
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
 bootstrap();
-=======
-
-  // Instatiate the validation pipes
-  app.useGlobalPipes(new ValidationPipe({
-    // Only needed data is received
-    whitelist: true
-  }));
-  app.enableCors();
-  // Start NestJS application
-  await app.listen(4000);
-}
-bootstrap();
->>>>>>> :construction: Adding basic dev/prod configs
