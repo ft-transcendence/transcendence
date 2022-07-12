81d74bbea4555ebcb6cfee0ea10b7c38fb64d919
shlu <shuyenlu@gmail.com>
2022-07-12 17:28:24 +0200

    :consturction: basic layout done

--- front/src/routes/chat_modes/chatRoom.tsx
+++ front/src/routes/chat_modes/chatRoom.tsx
@@ -5,6 +5,34 @@ import { chatPreview } from "./type/chat.type";
 export default function ChatRoom({current}:{current: chatPreview | undefined}) {
     console.log(current);
     return(
-        <div className="chat-room">chat room</div>
+        <div className="chat-room-zone">
+            <BriefInfo info= {current}/>
+            <MsgStream info= {current}/>
+            <InputArea/>
+        </div>
+    )
+}
+
+function BriefInfo({info}:{info: chatPreview | undefined}) {
+    return (
+        <div className="brief-info">
+
+        </div>
+    )
+}
+
+function MsgStream({info}:{info: chatPreview | undefined}) {
+    return (
+        <div className="msg-stream">
+
+        </div>
+    )
+}
+
+function InputArea() {
+    return (
+        <div className="msg-input-area">
+
+        </div>
     )
 }
--- front/src/routes/chat_modes/roomStatus.tsx
+++ front/src/routes/chat_modes/roomStatus.tsx
@@ -1,7 +1,27 @@
 import "./roomStatus.css";
+import { chatPreview } from "./type/chat.type";
 
-export default function RoomStatus() {
+export default function RoomStatus({current}:{current: chatPreview | undefined}) {
     return(
-        <div className="chat-status">room status</div>
+        <div className="chat-status-zone">
+            <InRoomSearch/>
+            <MemberStatus/>
+        </div>
+    )
+}
+
+function InRoomSearch() {
+    return (
+        <div className="in-room-search">
+
+        </div>
+    )
+}
+
+function MemberStatus() {
+    return (
+        <div className="member-status">
+
+        </div>
     )
 }
