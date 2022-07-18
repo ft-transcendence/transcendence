dae414dc42df24e9ba069b62d4052a63c23d50f2
sshakya <sshakya@student.42.fr>
2022-07-18 11:21:58 +0200

    :construction: Add Decorators
    - comment Decorators
    - @GetCurrentUser - return the user object
    - @GetCurrentUserId - return userId
    - @Public - disable JWT auth for route
    
    - The GetCurrent* decators will work for the protected routes
    - Allow for cleaner code in Functions
    *

--- back/src/auth/interfaces/42.interface.ts
+++ back/src/auth/interfaces/42.interface.ts
@@ -1,3 +1,7 @@
+/**
+ * Interface to extract user data from 42 API
+ */
+
 export interface Profile_42 {
     username: string;
     email: string;
--- back/src/decorators/get-current-user-decorator-id.ts
+++ back/src/decorators/get-current-user-decorator-id.ts
@@ -1,8 +1,12 @@
+/* GLOBAL MODULES */
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
+// Create @GetCurrentUser decorator - used to get current userID
 export const GetCurrentUserId = createParamDecorator(
     (data: undefined, context: ExecutionContext) => {
+        // Get request from context
         const request = context.switchToHttp().getRequest();
+        // Extract userID from request
         return request.user.sub;
     },
 );
