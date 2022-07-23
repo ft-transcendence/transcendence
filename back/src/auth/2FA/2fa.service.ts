a85cf0948623485ad3ebb3e17081d4202ff14f42
sshakya <sshakya@student.42.fr>
2022-07-23 13:05:17 +0200

    :contruction: 2FA JSON QR code generated
    - Create QR code using otplib and qrcode
    - paste response in browser to see QR code
    - scan in AuthApp to get code
    - Add route in controller
    *

--- back/src/auth/2FA/2fa.service.ts
+++ back/src/auth/2FA/2fa.service.ts
@@ -1,61 +1,93 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { authenticator } from 'otplib';
-import { GetCurrentUser } from 'src/decorators';
+import { GetCurrentUserId } from 'src/decorators';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { toDataURL } from 'qrcode';
+import { toDataURL, toFileStream } from 'qrcode';
 import { TwoFactorDto } from '../dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 /**
  * TWO FACTOR AUTHENTICATION SERVICE
  */
 @Injectable()
 export class TwoFactorService {
-	constructor(private prisma: PrismaService) {}
+	constructor(
+		private prisma: PrismaService,
+		private jwtService: JwtService,
+	) {}
 
-	async generate2FA(@GetCurrentUser() user) {
+	// Turn on 2FA for existing user
+	async turn_on_2fa(dto: TwoFactorDto, otp: string) {
+		// destructure data
+		const { email, twoFAcode } = dto;
+		// Check is 2FA code is valid
+		const isValid = await this.verify2FA(twoFAcode, otp);
+		// If invalid, throw error 401
+		if (!isValid) throw new UnauthorizedException('Invalid 2FA code');
+		// Enable 2FA for user
+		await this.prisma.user.update({
+			where: { email: email },
+			data: { twoFA: true },
+		});
+	}
+
+	// Turn off 2FA for existing user
+	async turn_off_2fa(@GetCurrentUserId() userId: number) {
+		await this.prisma.user.update({
+			where: { id: userId },
+			data: { twoFA: false },
+		});
+	}
+
+	// Generate a new 2FA fur user
+	async generate2FA(user: TwoFactorDto) {
+		// destructure data
+		const { email } = user;
+		// Generate a 2FA secret
 		const secret = authenticator.generateSecret();
+		// Create a URL for the QR code
 		const onetimepathurl = authenticator.keyuri(
-			user.email,
-			'TEST_APP',
+			email,
+			process.env.MY_2FA_APP_NAME,
 			secret,
 		);
+		// Add the secret to the user
 		await this.prisma.user.update({
-			where: { email: user.email },
-			data: { onetimepathurl: secret },
+			where: { email: email },
+			data: { twoFAsecret: secret },
 		});
+		// LOG
+		//console.log('gen2FA', secret, onetimepathurl);
 		return {
 			secret,
 			onetimepathurl,
 		};
 	}
 
-	async generate2FAQRCode(onetimepathurl: string) {
-		return toDataURL(onetimepathurl);
+	async login_with_2fa(user: any) {
+		const data = {
+			id: user.id,
+			email: user.email,
+			istwoFA: true,
+		};
+		return {
+			id: user.id,
+			access_token: this.jwtService.sign(data),
+		};
 	}
 
-	async turn_on_2fa(dto: TwoFactorDto, otp: string) {
-		const { userId, twoFAcode } = dto;
-		const isValid = await this.verify2FA(twoFAcode, otp);
-		if (!isValid) {
-			throw new UnauthorizedException('Invalid 2FA code');
-		}
-		await this.prisma.user.update({
-			where: { id: userId },
-			data: { twoFA: true },
-		});
-	}
-
-	async turn_off_2fa(@GetCurrentUser() user) {
-		await this.prisma.user.update({
-			where: { id: user.id },
-			data: { twoFA: false },
-		});
-	}
-
+	// Verify 2FA code
 	async verify2FA(otp: string, onetimepathurl: string) {
 		return authenticator.verify({
 			token: onetimepathurl,
 			secret: otp,
 		});
+	}
+
+	// Generate a QR code for the user
+	async generate2FAQRCode(onetimepathurl: string) {
+		// Generate a QR code from the URL
+		return toDataURL(onetimepathurl);
 	}
 }
