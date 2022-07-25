269a2af54191d52370a7b514cb49b0fb87c71ab4
satcheen <sshakya@student.42.fr>
2022-07-25 21:06:26 +0200

    :construction: Add QR to front !!
    *
    - add code to front for QRcode

--- front/src/modals/MActivateTwoFA.tsx
+++ front/src/modals/MActivateTwoFA.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 
 const handleSubmit = (event: any) => {
@@ -9,6 +10,22 @@ const handleSubmit = (event: any) => {
 // Enable 2FA modal
 
 export function Activate2FA(props: any) {
+
+  const [image, setImage] = useState<string>("");
+
+  useEffect(() => {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Bearer " + localStorage.getItem("userToken"));
+    fetch("http://localhost:4000/auth/2fa/generate", {
+      method: "POST",
+      headers: myHeaders,
+      redirect: "follow",
+    })
+      .then((response) => response.json())
+      .then((data) => setImage(data))
+  }, []);
+
   return (
     <Modal
       {...props}
@@ -22,6 +39,11 @@ export function Activate2FA(props: any) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        {image?
+        <div className="profile-pic-inside">
+          <img src={image} alt="2FA" />
+        </div>
+    : <div>Loading...</div>}
       <Form.Group className="mb-3" controlId="formbasicString">
         <Form.Label> Enter Code </Form.Label>
         <Form.Control type="email" placeholder="6-digit-code" />
