748b76cd5e9dc0b69df214811cfbdbeefd74c811
fcavillo <64540426+fcavillo@users.noreply.github.com>
2022-07-26 12:32:44 +0200

    Flo/feature/password functions (#35)
    * :construction: updateUser function protection
    
    - Protected updateUsername, updateAvatar and updateEmail with
    checkPassword
    
    * :construction: userDto ajusted to omit private
    
    - getUser and getFriends now return UserDtos without hash or token
    - using plainToClass from class-transformer to convert it
    
    * :construction: added get_blocked
    
    - Return a list of blocked user dtos
    
    * :construction: getPending
    
    - Returns a list of users that were invited
    
    * :construction: protected relation functions
    
    - Relation functions are now protected against mutiple adds/self adds
    
    * :broom: cleaned code
    
    *
    
    * :construction: updateUser function protection
    
    - Protected updateUsername, updateAvatar and updateEmail with
    checkPassword
    
    * :construction: userDto ajusted to omit private
    
    - getUser and getFriends now return UserDtos without hash or token
    - using plainToClass from class-transformer to convert it
    
    * :construction: added get_blocked
    
    - Return a list of blocked user dtos
    
    * Update user.controller.ts
    
    * :construction: getPending
    
    - Returns a list of users that were invited
    
    * :construction: protected relation functions
    
    - Relation functions are now protected against mutiple adds/self adds
    
    * :broom: cleaned code
    
    *
    
    * :broom: Eslint formatting
    
    *
    
    * :hammer: added error management
    
    - getUser, getMe, and other findUnique using functions are now protected
    by some exceptions
    
    * :broom: Eslint cleaning
    
    *
    
    * :bug: check_password id reading error
    
    - getCurrentUserId() was not working in check_password, changed it with
    request.user.id
    
    Co-authored-by: Marie-Lou Valdes <57663880+mvaldes42@users.noreply.github.com>

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,184 +1,242 @@
-import { Body, Controller,
-	Get,
-	Post,
-	Req,
-	UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { hash } from 'argon2';
 import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
+import { GetCurrentUserId } from 'src/decorators';
 /* USER MODULES */
-import { UserService } from "./user.service";
+import { UserService } from './user.service';
 
 /*
-*	CRUD :
-*	- Create : Satch is doing the base user creation, maybe Flo has to initialize each var
-*	- Read : Flo's stuff, are getAll and getMe enough ? No, getUser should be made too to get a specific user and access his profile
-*	- Update : Flo's ugly stuff, has to understand how often and when an update happens
-*	- Delete : Necessary ? In which case would we delete just a user ?
-*/
+ *	CRUD :
+ *	- Create : Satch is doing the base user creation, maybe Flo has to initialize each var
+ *	- Read : Flo's stuff, are getAll and getMe enough ? No, getUser should be made too to get a specific user and access his profile
+ *	- Update : Flo's ugly stuff, has to understand how often and when an update happens
+ *	- Delete : Necessary ? In which case would we delete just a user ?
+ */
 
 @Controller('users')
 export class UserController {
-
-	constructor(private userService: UserService) {}    
+	constructor(private userService: UserService) {}
 
 	/*	READ	*/
 
-	@Get('me') 
-	getMe(@Req() request: Request) {
-			// log in console
-			console.log({
-				user: request.user,
-			})
-		const prismaUser  = (request.user as User);
-//		console.log(userE.id)
-		return this.userService.getUser(prismaUser.id);
-		// return request.user;
-	
+	@Get('me')
+	getMe(@Req() request) {
+		const userDto = this.userService.getUser(request.user.id);
+		return userDto;
 	}
 
-	@Get('him') //to change
-	getUser(id: number) {
-		console.log('Going through getUser in user.controller');
-		return this.userService.getUser(id);
-	}	
+	@Get('get_user')
+	getUser(@Body('otherId') otherId: number) {
+		const userDto = this.userService.getUser(otherId);
+		return userDto;
+	}
 
-	@Get('/')	//default testing route, localhost:4000/users/
+	@Get('/') //default testing route, localhost:4000/users/
 	getAllUsers() {
 		console.log('Going through getAllUsers in user.controller');
-		return this.userService.getAllUsers();
+		const userListDtos = this.userService.getAllUsers();
+		return userListDtos;
 	}
-	
+
 	@Get()
 	getLeaderboard() {
 		console.log('Going through getLeaderboard in user.controller');
-		return this.userService.getLeaderboard();        
+		return this.userService.getLeaderboard();
 	}
 
 	@Get('get_friends')
 	async getFriends(@Req() request) {
 		console.log('Going through getFriends in user.controller');
 		const result = await this.userService.getFriends(request.user.id);
-		return (result);
+		return result;
+	}
 
+	@Get('get_pending')
+	async getPending(@Req() request) {
+		console.log('Going through getPending in user.controller');
+		const result = await this.userService.getPending(request.user.id);
+		return result;
+	}
+
+	@Get('get_blocked')
+	async getBlocked(@Req() request) {
+		console.log('Going through getBlocked in user.controller');
+		const result = await this.userService.getBlocks(request.user.id);
+		return result;
 	}
 
 	@Get('is_friend')
 	async isFriend(@Req() request, @Body('otherId') otherId: number) {
 		console.log('Going through isFriend in user.controller');
-		const result = await this.userService.isFriend(request.user.id, otherId);
-		return (result);
+		const result = await this.userService.isFriend(
+			request.user.id,
+			otherId,
+		);
+		return result;
 	}
 
 	@Get('is_blocked')
 	async isBlocked(@Req() request, @Body('otherId') otherId: number) {
 		console.log('Going through isFriend in user.controller');
-		const result = await this.userService.isBlocked(request.user.id, otherId);
-		return (result);
+		const result = await this.userService.isBlocked(
+			request.user.id,
+			otherId,
+		);
+		return result;
 	}
 
 	@Post('check_password')
 	async checkPassword(@Body('password') password: string, @Req() request) {
 		console.log('Going through checkPword in user.controller');
-		const prismaUser  = (request.user as User);
-		const fullUser = await this.userService.getUser(prismaUser.id);
-		const result = await this.userService.checkPassword(fullUser.id, password);
-		return (result);
+		const result = await this.userService.checkPassword(
+			request.user.id,
+			password,
+		);
+		return result;
 	}
-
 
 	/*	UPDATE	*/
 
 	//USER PROFILE RELATED FUNCTIONS
 
 	@Post('/update_username')
-	async updateUsername(@Body('username') newUsername: string, @Req() request) {
+	async updateUsername(
+		@Body('username') newUsername: string,
+		@Body('password') password: string,
+		@Req() request,
+	) {
 		console.log('Going through updateUsername in user.controller');
-		const result = await this.userService.updateUsername(request.user.id, newUsername);
-		return (result);
+		const result = await this.userService.updateUsername(
+			request.user.id,
+			newUsername,
+			password,
+		);
+		return result;
 	}
 
 	@Post('/update_avatar')
-	async updateAvatar(@Body('avatar') newAvatar: string, @Req() request) {
+	async updateAvatar(
+		@Body('avatar') newAvatar: string,
+		@Body('password') password: string,
+		@Req() request,
+	) {
 		console.log('Going through updateAvatar in user.controller');
-		const result = await this.userService.updateAvatar(request.user.id, newAvatar);
-		return (result);
+		const result = await this.userService.updateAvatar(
+			request.user.id,
+			newAvatar,
+			password,
+		);
+		return result;
 	}
 
 	@Post('/update_email')
-	async updateEmail(@Body('email') newEmail: string, @Req() request) {
+	async updateEmail(
+		@Body('email') newEmail: string,
+		@Body('password') password: string,
+		@Req() request,
+	) {
 		console.log('Going through updateEmail in user.controller');
-		const result = await this.userService.updateEmail(request.user.id, newEmail);
-		return (result);
+		const result = await this.userService.updateEmail(
+			request.user.id,
+			newEmail,
+			password,
+		);
+		return result;
 	}
-
-
 
 	//RELATIONSHIP RELATED FUNCTIONS
 
 	@Post('/add_friend')
-	async addFriend(@Req() request, @Body('otherId') otherId: number){
-//		console.log('Going through addFriend in user.controller: ' + request.user.id + ' -> ' + otherId);
-		const result = await this.userService.addFriend(request.user.id, otherId);
+	async addFriend(@Req() request, @Body('otherId') otherId: number) {
+		//		console.log('Going through addFriend in user.controller: ' + request.user.id + ' -> ' + otherId);
+		const result = await this.userService.addFriend(
+			request.user.id,
+			otherId,
+		);
 
-		return (result);
+		return result;
 	}
 
 	@Post('/rm_friend')
-	async rmFriend(@Req() request, @Body('otherId') otherId: number){
-//		console.log('Going through addFriend in user.controller: ' + request.user.id + ' -> ' + otherId);
-		const result = await this.userService.rmFriend(request.user.id, otherId);
+	async rmFriend(@Req() request, @Body('otherId') otherId: number) {
+		//		console.log('Going through addFriend in user.controller: ' + request.user.id + ' -> ' + otherId);
+		const result = await this.userService.rmFriend(
+			request.user.id,
+			otherId,
+		);
 
-		return (result);
+		return result;
 	}
 
 	@Post('/cancel_invite')
-	async cancelInvite(@Req() request, @Body('otherId') otherId: number){
-//		console.log('Going through cancelInvite in user.controller: ' + request.user.id + ' -> ' + otherId);
-		const result = await this.userService.cancelInvite(request.user.id, otherId);
-		return (result);		
+	async cancelInvite(@Req() request, @Body('otherId') otherId: number) {
+		//		console.log('Going through cancelInvite in user.controller: ' + request.user.id + ' -> ' + otherId);
+		const result = await this.userService.cancelInvite(
+			request.user.id,
+			otherId,
+		);
+		return result;
 	}
 
 	@Post('/deny_invite')
-	async denyInvite(@Req() request, @Body('otherId') otherId: number){
-//		console.log('Going through denyInvite in user.controller: ' + request.user.id + ' -> ' + otherId);
-		const result = await this.userService.denyInvite(request.user.id, otherId);
-		return (result);		
+	async denyInvite(@Req() request, @Body('otherId') otherId: number) {
+		//		console.log('Going through denyInvite in user.controller: ' + request.user.id + ' -> ' + otherId);
+		const result = await this.userService.denyInvite(
+			request.user.id,
+			otherId,
+		);
+		return result;
 	}
 
 	@Post('/block_user')
-	async blockUser(@Req() request, @Body('otherId') otherId: number){
-		console.log('Going through blockUser in user.controller: ' + request.user.id + ' -> ' + otherId);
-		const result = await this.userService.blockUser(request.user.id, otherId);
+	async blockUser(@Req() request, @Body('otherId') otherId: number) {
+		console.log(
+			'Going through blockUser in user.controller: ' +
+				request.user.id +
+				' -> ' +
+				otherId,
+		);
+		const result = await this.userService.blockUser(
+			request.user.id,
+			otherId,
+		);
 
-		return (result);
+		return result;
 	}
 
 	@Post('/unblock_user')
-	async unblockUser(@Req() request, @Body('otherId') otherId: number){
-		console.log('Going through unblockUser in user.controller: ' + request.user.id + ' -> ' + otherId);
-		const result = await this.userService.unblockUser(request.user.id, otherId);
+	async unblockUser(@Req() request, @Body('otherId') otherId: number) {
+		console.log(
+			'Going through unblockUser in user.controller: ' +
+				request.user.id +
+				' -> ' +
+				otherId,
+		);
+		const result = await this.userService.unblockUser(
+			request.user.id,
+			otherId,
+		);
 
-		return (result);
+		return result;
 	}
 
 	//GAME RELATED FUNCTIONS
 
-//this is not a request - it comes from the back, called by the game, no need to protect
+	//this is not a request - it comes from the back, called by the game, no need to protect
 	hasWon(id: number) {
 		console.log('Going through hasWon in user.controller for');
-		return (this.userService.hasWon(id));
+		return this.userService.hasWon(id);
 	}
 
 	hasLost(id: number) {
 		console.log('Going through hasLost in user.controller for');
-		return (this.userService.hasLost(id));
+		return this.userService.hasLost(id);
 	}
 
 	hadADraw(id: number) {
 		console.log('Going through hasWon in user.controller for');
-		return (this.userService.hadADraw(id));
+		return this.userService.hadADraw(id);
 	}
 }
