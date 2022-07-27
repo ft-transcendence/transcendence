836d3ba1b4e420ac65eb331b3f7d795c21b08113
satcheen <sshakya@student.42.fr>
2022-07-27 17:09:25 +0200

    :construction: add 2FA route to front (mvaldes)
    *
    - work in progress

---
+++ front/src/routes/TwoFAValidation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+
+interface test
+
+export default function TwoFAValidation() { 
+    let ref = React.createRef();
+
+
+
+    return (
+      <div>
+        <Form.Group className="mb-3">
+            <Form.Label className="form-test ">CODE 2FA</Form.Label>
+            <Form.Control
+                ref={ref}
+              type="text"
+            />
+            <Button
+            variant="primary"
+            type="submit"
+            className="submit-button"
+            size="sm"
+          >
+            Submit
+          </Button>
+          </Form.Group>
+      </div>
+    );
+  }
