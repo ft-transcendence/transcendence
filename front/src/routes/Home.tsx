c734931062a1899db06a84784ac29b9a19f5c6cc
Marie-lou VALDES <mvaldes@student.42.fr>
2022-07-28 17:50:54 +0200

    🚧 wip toolbar for app
    *

--- front/src/routes/Home.tsx
+++ front/src/routes/Home.tsx
@@ -1,11 +1,18 @@
-import { Col, Row } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import IconHome from "../ressources/icons/Icon_Home.svg";
+import IconChat from "../ressources/icons/Icon_Chat.svg";
+import IconFight from "../ressources/icons/Icon_Fight.svg";
+import IconFriends from "../ressources/icons/Icon_Friends.svg";
+import IconLeaderboard from "../ressources/icons/Icon_Leaderboard.svg";
+import IconWatch from "../ressources/icons/Icon_Watch.svg";
+
+import "./Home.css";
 
 export default function Home() {
   return (
-    <main style={{height:'100%'}}>
-      <h2>App</h2>
-      <nav
+    <main className="Home">
+      {/* <h2>App</h2> */}
+      {/* <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
@@ -16,10 +23,28 @@ export default function Home() {
         <Link to="/app/leader-board">Leader Board</Link> |{"  "}
         <Link to="/app/game">Game</Link> |{"  "}
         <Link to="/app/watch">Watch</Link>
-      </nav>
-      <div className="container-sm border">
+      </nav> */}
+      <div className="toolbar">
+        <div className="toolbar-top space-around ">
+          <input type="image" alt="Home" src={IconHome} className="icons" />
+          <input
+            type="image"
+            alt="Friends"
+            src={IconFriends}
+            className="icons"
+          />
+          <input
+            type="image"
+            alt="Leaderboard"
+            src={IconLeaderboard}
+            className="icons"
+          />
+          <input type="image" alt="Chat" src={IconChat} className="icons" />
+          <input type="image" alt="Fight" src={IconFight} className="icons" />
+          <input type="image" alt="Watch" src={IconWatch} className="icons" />
+        </div>
       </div>
-      {/* <Outlet></Outlet> */}
+      <Outlet></Outlet>
     </main>
   );
 }
--- front/src/routes/profile_types/UserPrivateProfile.tsx
+++ front/src/routes/profile_types/UserPrivateProfile.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import { ModifyEntry } from "./ModifyUserInfo";
-import IconPen from "../../ressources/icons/IconPen.svg";
+import IconPen from "../../ressources/icons/Icon_Pen.svg";
 import { MUploadAvatar } from "../../modals/MUploadAvatar";
 //import { UsersRelations } from "./FriendsList";
 import { Activate2FA } from "../../modals/MActivateTwoFA";
