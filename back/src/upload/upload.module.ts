4bb3b1db46579f771bab6f0a308c63098adc44a8
satcheen <sshakya@student.42.fr>
2022-08-09 12:53:15 +0200

    :construction: download from 42 API
    *
    - user axios to download avatar from 42 cnd
    - WIP

--- back/src/upload/upload.module.ts
+++ back/src/upload/upload.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
 import { UserService } from 'src/user/user.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+	imports: [HttpModule],
 	providers: [UploadService, UserService],
 	controllers: [UploadController],
 })
--- back/src/upload/upload.service.ts
+++ back/src/upload/upload.service.ts
@@ -1,4 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { createReadStream } from 'node:fs';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+	constructor(private readonly httpService: HttpService) {}
+	async download(
+		url: string,
+		userId: number,
+	): Observable<AxiosResponse<any>> {
+		const file = createReadStream(url);
+	}
+}
