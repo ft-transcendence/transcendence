9c8324aa9858cc42df6c081a67bd41a5578e2f8e
shlu <shuyenlu@gmail.com>
2022-08-11 14:43:10 +0200

    :waste_basket: rm unused useEffect

--- front/src/routes/LeaderBoard.tsx
+++ front/src/routes/LeaderBoard.tsx
@@ -1,7 +1,5 @@
 import "./LeaderBoard.css"
 import { useEffect, useState } from "react";
-import { gsap } from "gsap";
-
 
 export default function LeaderBoard() {
     const [data, setData] = useState<[]>([]);
@@ -9,69 +7,6 @@ export default function LeaderBoard() {
     useEffect(() => {
         setData(JSON.parse(localStorage.getItem("leaderBoard")!));
     }, [])
-
-
-
-
-    useEffect(() => {
-
-        var tl = gsap.timeline({
-            repeat: -1,
-            repeatDelay: 2.5,
-            defaults: { ease: "power2.inOut" }
-          });
-          tl.set(".word.leader, .word.board", { opacity: 0 });
-          // tl.set(".bottom-side", )
-          
-        //   const bgColor = "hsl(190, 100%, 75%)";
-          
-          // Lines
-          tl.fromTo(
-            ".left-side",
-            {
-              height: 0,
-              immediateRender: false,
-              autoRound: false
-            },
-            {
-              duration: 2.3,
-              height: "100%",
-              ease: "power3.in"
-            },
-            0
-          );
-          tl.fromTo(
-            ".bottom-side",
-            {
-              width: 0,
-          
-              immediateRender: false,
-              autoRound: false
-            },
-            {
-              duration: 2.3,
-              width: "100%",
-              ease: "power3.out"
-            },
-            2.3
-          );
-          
-          // TOO
-          tl.fromTo(
-            ".leader",
-            { opacity: 0 },
-            { duration: 1.3, opacity: 1, stagger: 0.06 },
-            "-=1"
-          );
-          tl.fromTo(
-            ".board",
-            { opacity: 0 },
-            { duration: 1.3, opacity: 1, stagger: 0.06 },
-            "-=0.6"
-          );
-          
-    }, []);
-
 
     return (
         <div className="background">
@@ -81,17 +16,6 @@ export default function LeaderBoard() {
             </div>
             <div className="block"/>
             <div className="leaderboard">
-                {/* <div className="header">
-                    <OneRow
-                        id={"id"}
-                        avatar={""}
-                        username={"user"}
-                        gamesWon={"won"}
-                        gamesLost={"lost"}
-                        gamesPlayed={"played"}
-                        head={true}
-                    />
-                </div> */}
                 <div className="list">
                     {
                         data?.map((one:any, index) => {
