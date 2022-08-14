f039254819d6fa963a9ebde44aa6c54281c62e3d
Marie-lou VALDES <mvaldes@student.42.fr>
2022-08-14 23:47:14 +0200

    ➕ icons style for navbar
    using bootstrap icons classes

--- front/src/Components/Navbar.tsx
+++ front/src/Components/Navbar.tsx
@@ -1,84 +1,66 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../globals/contexts";
 import "./Navbar.css";
-import * as Icon from "react-bootstrap-icons";
 
-export const CNavBar = () => {
-  let auth = useAuth();
+const GetIcons = (props: any) => {
   const navigate = useNavigate();
   const location = useLocation();
+  let auth = useAuth();
 
+  const url = props.url;
+  const fill =
+    url === "private-profile"
+      ? "person"
+      : url === "leaderboard"
+      ? "trophy"
+      : url === "chat"
+      ? "chat-left-dots"
+      : url === "game"
+      ? "dpad"
+      : url === "watch"
+      ? "caret-right-square"
+      : "box-arrow-right";
+
+  return (
+    <div>
+      <i
+        id="clickableIcon"
+        className={`bi bi-${fill} icons thick ${
+          location.pathname === "/app/" + url ? "hide" : "current"
+        }`}
+        onClick={
+          url === "logout"
+            ? () => {
+                auth.signout(() => navigate("/"));
+              }
+            : () => {
+                navigate("/app/" + url);
+              }
+        }
+      />
+      <i
+        id="clickableIcon"
+        className={`bi bi-${fill}-fill icons thin ${
+          location.pathname === "/app/" + url ? "current" : "hide"
+        }`}
+        onClick={() => {
+          navigate("/app/" + url);
+        }}
+      />
+    </div>
+  );
+};
+
+export const CNavBar = () => {
   return (
     <div className="toolbar col-">
       <div className="toolbar-top space-around">
-        <Icon.PersonFill
-          id="clickableIcon"
-          className={`icons thick ${
-            location.pathname === "/app/private-profile" ? "hide" : "current"
-          }`}
-          onClick={() => {
-            navigate("/app/private-profile");
-          }}
-        />
-        <Icon.Person
-          id="clickableIcon"
-          className={`icons thin ${
-            location.pathname === "/app/private-profile" ? "current" : "hide"
-          }`}
-          onClick={() => {
-            navigate("/app/private-profile");
-          }}
-        />
-        <span id="clickableIcon">
-          <i
-            className={`bi bi-trophy icons ${
-              location.pathname === "/app/leaderboard" ? "clicked" : "none"
-            }`}
-            onClick={() => {
-              navigate("/app/leaderboard");
-            }}
-          ></i>
-        </span>
-        <span id="clickableIcon">
-          <i
-            className={`bi bi-chat-left-dots icons ${
-              location.pathname === "/app/chat" ? "clicked" : "none"
-            }`}
-            onClick={() => {
-              navigate("/app/chat");
-            }}
-          ></i>
-        </span>
-        <span id="clickableIcon">
-          <i
-            className={`bi bi-controller icons ${
-              location.pathname === "/app/game" ? "clicked" : "none"
-            }`}
-            onClick={() => {
-              navigate("/app/game");
-            }}
-          ></i>
-        </span>
-        <span id="clickableIcon">
-          <i
-            className={`bi bi-caret-right-square icons ${
-              location.pathname === "/app/watch" ? "clicked" : "none"
-            }`}
-            onClick={() => {
-              navigate("/app/watch");
-            }}
-          ></i>
-        </span>
-        <span id="clickableIcon">
-          <i
-            className={`bi bi-box-arrow-right icons ${
-              location.pathname === "/app/private-profile" ? "clicked" : "none"
-            }`}
-            onClick={() => {
-              auth.signout(() => navigate("/"));
-            }}
-          ></i>
-        </span>
+        <GetIcons url="private-profile" />
+        <GetIcons url="leaderboard" />
+        <GetIcons url="chat" />
+        <GetIcons url="game" />
+        <GetIcons url="watch" />
+        <GetIcons url="logout" />
       </div>
     </div>
   );
