f0ba41bd7d239b35f2a1abf6736a8b347b70865d
Marie-lou VALDES <mvaldes@student.42.fr>
2022-08-21 20:42:21 +0200

    ➕ conditional rendering of profile
    based on if loading or user doesn't exist.

--- front/src/routes/profile_types/UserPublicProfile.tsx
+++ front/src/routes/profile_types/UserPublicProfile.tsx
@@ -35,6 +35,7 @@ const initializeUser = (result: any, setUserInfo: any) => {
   userInfoInit.rank = result.rank;
   userInfoInit.score = result.score;
   userInfoInit.winRate = result.winRate;
+  console.log("result.winRate", result.winRate);
   setUserInfo(userInfoInit);
 };
 
@@ -43,6 +44,7 @@ export default function UserProfile() {
   const [userInfo, setUserInfo] = useState<userModel>(userInfoInit);
   const [isFetched, setIsFetched] = useState(false);
   const [avatarURL, setAvatarURL] = useState("");
+  const [isUser, setIsUser] = useState(true);
   // const [avatarFetched, setAvatarFetched] = useState(false);
 
   useEffect(() => {
@@ -63,9 +65,11 @@ export default function UserProfile() {
       let result;
       if (!isFetched && params.userName !== undefined) {
         result = await getOtherUser(+params.userName);
-        console.log("result: ", result);
-        initializeUser(result, setUserInfo);
-        setIsFetched(true);
+        if (result !== "error") {
+          console.log("result: ", result);
+          initializeUser(result, setUserInfo);
+          setIsFetched(true);
+        } else setIsUser(false);
       }
     };
     fetchIsUser();
@@ -74,82 +78,86 @@ export default function UserProfile() {
 
   return (
     <main>
-      <h1 className="app-title border">My account</h1>
-      <Container className="p-5 border">
-        <Row className="wrapper public-profile-header">
-          <div className="p-2 public-profile-round">
-            <div
-              className="profile-pic-inside"
-              style={{
-                backgroundImage: `url("${avatarURL}")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-            ></div>
-          </div>
-          <Col className="content">
-            <div className="public-username-text">@{userInfo.username}</div>
-            <div className="public-rank-text"> Rank #</div>
-          </Col>
-          <Col>
-            <div id="clickableIcon" className="buttons-round-big float-end">
-              <i className="bi bi-dpad-fill big-icons" />
-            </div>
-            <div id="clickableIcon" className="buttons-round-big float-end">
-              <i className="bi bi-caret-right-square-fill big-icons" />
-            </div>
-            <div id="clickableIcon" className="buttons-round-big float-end">
-              <i className="bi bi-chat-left-dots-fill big-icons" />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-
-      <Container className="p-5 text-center">
-        <Row
-          className="ROBOTO-text"
-          style={{ fontSize: "20px", fontWeight: "400" }}
-        >
-          <Col>Win Rate</Col>
-          <Col>Total Win</Col>
-          <Col>Play Time</Col>
-        </Row>
-        <Row className="IBM-text text-huge">
-          <Col>{userInfo.winRate}</Col>
-          <Col>{userInfo.gamesWon}</Col>
-          <Col>{Math.floor(userInfo.playTime / 1000)}s</Col>
-        </Row>
-      </Container>
-      <Container className="p-5">
-        <Row className="flex">
-          <Col className="col-6">
-
-            <Card className="p-3 public-card">
-              <Card.Body>
-                <Row className="wrapper p-1">
-                  <Col className="text-wrapper">
-                    <div
-                      className="IBM-text"
-                      style={{ fontSize: "20px", fontWeight: "500" }}
-                    >
-                      Latest Games
-                    </div>
-                  </Col>
-                  <Col className="">
-                    <div
-                      className="IBM-text float-end"
-                      style={{ fontSize: "20px", fontWeight: "500" }}
-                    >
-                      {userInfo.gamesLost + userInfo.gamesWon}
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-            
-          </Col>
-        </Row>
-      </Container>
+      {isUser && isFetched ? (
+        <main>
+          {" "}
+          <h1 className="app-title border">My account</h1>
+          <Container className="p-5 border">
+            <Row className="wrapper public-profile-header">
+              <div className="p-2 public-profile-round">
+                <div
+                  className="profile-pic-inside"
+                  style={{
+                    backgroundImage: `url("${avatarURL}")`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                ></div>
+              </div>
+              <Col className="content">
+                <div className="public-username-text">@{userInfo.username}</div>
+                <div className="public-rank-text"> Rank #</div>
+              </Col>
+              <Col>
+                <div id="clickableIcon" className="buttons-round-big float-end">
+                  <i className="bi bi-dpad-fill big-icons" />
+                </div>
+                <div id="clickableIcon" className="buttons-round-big float-end">
+                  <i className="bi bi-caret-right-square-fill big-icons" />
+                </div>
+                <div id="clickableIcon" className="buttons-round-big float-end">
+                  <i className="bi bi-chat-left-dots-fill big-icons" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <Container className="p-5 text-center">
+            <Row
+              className="ROBOTO-text"
+              style={{ fontSize: "20px", fontWeight: "400" }}
+            >
+              <Col>Win Rate</Col>
+              <Col>Total Win</Col>
+              <Col>Play Time</Col>
+            </Row>
+            <Row className="IBM-text text-huge">
+              <Col>{userInfo.winRate}</Col>
+              <Col>{userInfo.gamesWon}</Col>
+              <Col>{Math.floor(userInfo.playTime / 1000)}s</Col>
+            </Row>
+          </Container>
+          <Container className="p-5">
+            <Row className="flex">
+              <Col className="col-6">
+                <Card className="p-3 public-card">
+                  <Card.Body>
+                    <Row className="wrapper p-1">
+                      <Col className="text-wrapper">
+                        <div
+                          className="IBM-text"
+                          style={{ fontSize: "20px", fontWeight: "500" }}
+                        >
+                          Latest Games
+                        </div>
+                      </Col>
+                      <Col className="">
+                        <div
+                          className="IBM-text float-end"
+                          style={{ fontSize: "20px", fontWeight: "500" }}
+                        >
+                          {userInfo.gamesLost + userInfo.gamesWon}
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </main>
+      ) : isUser && !isFetched ? null : (
+        <main>User does not exist.</main>
+      )}
     </main>
   );
 }
